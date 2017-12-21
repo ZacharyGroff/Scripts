f28f5ebe247b700f2db33d19e33f957e0a52e290
Zachary Groff <groff.zackm@gmail.com>
2017-12-21 10:41:44 -0500

    Added dir_search script

---
+++ dir_search.py
@@ -0,0 +1,43 @@
+'''
+This script was created to search directories
+   for all files modified in a day.
+Two files (filelog.txt & found.txt) are created
+    in the same directory that holds this file.
+'''
+
+import os
+import datetime
+
+f = open('filelog.txt', 'w+')
+print('What directory would you like to search?')
+root = str(input())
+
+# search directories starting at user given root
+#   and write paths to file_list
+for subdir, dirs, files in os.walk(root):
+    for file in files:
+        f.write(str(os.path.join(subdir, file)) + '\n')
+
+file_list = open('filelog.txt').read().splitlines()
+current_date = str(datetime.date.today())
+found_log = open('found.txt', 'w+')
+files_found = 0
+
+# check if each file in file_list has been modified today
+#   and write it to found_log
+for path in file_list:
+    try:
+        
+        access_date = str(datetime.date.fromtimestamp(
+            os.path.getmtime(path)))
+        
+        if current_date == access_date:
+            
+            found_log.write(path + '\naccessed:\t' 
+                    + access_date + '\n')
+            files_found += 1
+    
+    except (PermissionError, FileNotFoundError):
+        continue
+
+print('{} files found modified today in {}'.format(files_found, root))
